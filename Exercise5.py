daeec3cafdce341e859c5152ed4c48178cb63e6a
Daryl Mizrahi <darylmizrahi@gmail.com>
2020-04-06 21:51:53 +0300

    Ex 5

---
+++ Exercise5.py
@@ -0,0 +1,14 @@
+print("When would the use of generators be preferred over list comprehension and vice versa ?")
+
+print("It depends on what we do in our code :")
+print("-------------------------")
+print("We need to use List comprehension if speed matters and we have a lot of resources.")
+print("A List Comprehension executes immediately and returns a list")
+print("-------------------------")
+print("We need to use Generator if speed does not matter and we are limited in resources.")
+print("A Generator Expression, just like range returns and object that can be iterated over.")
+print("-------------------------")
+print("Generators and list comprehension do approximately the same thing but generators are slower than the lists")
+print("-------------------------")
+print("The difference between the two kinds of expressions is that the List comprehension is enclosed in square "
+      "brackets [] while the Generator expression is enclosed in  parentheses ().")
