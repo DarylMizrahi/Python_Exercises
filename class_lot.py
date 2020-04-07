196e493762839bb54e1cb46cfe7b4ae70b1c142f
Daryl Mizrahi <darylmizrahi@gmail.com>
2020-04-07 19:05:36 +0300

    ex 2 to 4

---
+++ class_lot.py
@@ -0,0 +1,24 @@
+class car_lot:
+    floors = 0
+    total_spots = 0
+
+    def __init__(self, floors, total_spots):
+        self.floors = floors
+        self.total_spots = total_spots
+
+    def set_floors(self, floors_val):
+        self.floors = floors_val
+
+    def set_total_spots(self, total_spots_val):
+        self.total_spots = total_spots_val
+
+    def get_floors(self):
+        return self.floors
+
+    def get_total_spots(self):
+        return self.total_spots
+
+mycar_lot = car_lot(3, 30)
+print(mycar_lot.get_total_spots())
+
+
